a2de85874722062fd401772a35b897e9eef53ba3
Bo Peng <pb0928@gmail.com>
2025-05-24 01:25:47 -0400

    update plugins

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,11 +1,12 @@
 require("config.lazy")
 require("config.vim")
+require("config.keymaps")
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
+    spec = {
+        -- import your plugins
+        { import = "plugins" },
+    },
 })
 
--- home/dot_config/nvim/lua/plugins/oil.lua
+++ home/dot_config/nvim/lua/plugins/oil.lua
@@ -1,17 +1,14 @@
 return {
-  "stevearc/oil.nvim",
-  dependencies = {
-    { "echasnovski/mini.icons", lazy = false },
-    { "nvim-tree/nvim-web-devicons" }
-  },
-  config = function()
-    local oil = require("oil")
-    oil.setup({
-      view_options = {
-        show_hidden = true,
-      },
-    })
-    vim.keymap.set("n", "-", oil.toggle_float, {})
-  end,
-  lazy = false,
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+        view_options = {
+            show_hidden = true,
+        },
+    },
+    dependencies = {
+        { "echasnovski/mini.icons", lazy = false },
+    },
+    lazy = false,
 }
