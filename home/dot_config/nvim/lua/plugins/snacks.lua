c679aeb7b0ae6083f28c4055b568c20598d166ad
Bo Peng <29678468+bopeng95@users.noreply.github.com>
2025-06-08 19:05:54 -0400

    update snacks

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -5,29 +5,7 @@ return {
     lazy = false,
     opts = {
       indent = { enabled = true },
-      rename = { enabled = true },
-      dashboard = {
-        enabled = true,
-        sections = {
-          { section = "header" },
-          {
-            pane = 2,
-            section = "terminal",
-            cmd = "colorscript -e square",
-            height = 5,
-            padding = 1,
-          },
-          {
-            section = "terminal",
-            cmd = "pokemon-colorscripts --no-title -n swampert; sleep .1",
-            pane = 2,
-            indent = 4,
-            height = 30
-          },
-          { section = "keys", gap = 1, padding = 1 },
-          { section = "startup" },
-        },
-      },
+      dashboard = { enabled = true },
     },
     keys = {
       -- NOTE: lazy git
