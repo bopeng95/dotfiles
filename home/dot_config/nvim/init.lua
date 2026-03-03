ab8c355d004eb34675129fcc8301305449b38161
Bo Peng <pb0928@gmail.com>
2026-03-03 14:58:07 -0500

    lazyvim

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,12 +1,2 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-require("config.vim")
-require("config.keymaps")
-
--- Setup lazy.nvim
-require("lazy").setup({
-    spec = {
-        -- import your plugins
-        { import = "plugins" },
-    },
-})
-
--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -1,30 +1,3 @@
--- map leader keys
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
-
--- format without prettier using the built in
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
--- keep lines highlighted when indenting
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
-
--- Replace the word cursor is on globally
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
-    { desc = "Replace word cursor is on globally" })
-
--- Copy filepath to the clipboard
-vim.keymap.set("n", "<leader>fp", function()
-  local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
-  vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
-  print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
-end, { desc = "Copy file path to clipboard" })
-
-vim.keymap.set("n", "<leader>cf", function()
-  require("conform").format({
-    lsp_format = "fallback",
-  })
-end, { desc = "Format current file" })
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
