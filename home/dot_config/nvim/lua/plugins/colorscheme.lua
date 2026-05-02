2442ab3cac6d3c8b787253133ef6d948c0a075d1
Bo Peng <29678468+bopeng95@users.noreply.github.com>
2026-05-02 12:48:47 -0400

    update colorscheme

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,14 +3,9 @@ return {
   {
     "catppuccin/nvim",
     name = "catppuccin",
-    lazy = false,
     priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        flavour = "mocha",
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
+    opts = {
+      flavour = "mocha",
+    },
   },
 }
