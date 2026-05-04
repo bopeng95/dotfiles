2980177c1557037b8cc39c0f1feaf03829dbe742
Bo Peng <pb0928@gmail.com>
2026-05-04 13:03:26 -0400

    add new colorscheme and update oil

--- home/dot_config/nvim/lua/plugins/oil.lua
+++ home/dot_config/nvim/lua/plugins/oil.lua
@@ -12,14 +12,23 @@ return {
     view_options = {
       show_hidden = true,
     },
+    float = {
+      override = function(conf)
+        conf.width = math.floor(vim.o.columns * 0.8)
+        conf.height = math.floor(vim.o.lines * 0.8)
+        conf.col = math.floor((vim.o.columns - conf.width) / 2)
+        conf.row = math.floor((vim.o.lines - conf.height) / 2)
+        return conf
+      end,
+    },
   },
   keys = {
     {
       "-",
       mode = "n",
       function()
-        require("oil").open()
-        -- require("oil").open_float()
+        -- require("oil").open()
+        require("oil").open_float()
       end,
       desc = "Open parent directory",
     },
