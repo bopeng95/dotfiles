f60f8a573c48cdc5b4e743165fbc4af2eae0d3d6
Bo Peng <29678468+bopeng95@users.noreply.github.com>
2026-05-04 13:03:26 -0400

    add new colorscheme and update oil

--- home/dot_config/nvim/lua/plugins/colorscheme.lua
+++ home/dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
   -- NOTE: vague
-  {
-    "vague-theme/vague.nvim",
-    name = "vague",
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("vague")
-    end,
-  },
+  -- {
+  --   "vague-theme/vague.nvim",
+  --   name = "vague",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("vague")
+  --   end,
+  -- },
   -- NOTE: catppuccin
   -- {
   --   "catppuccin/nvim",
@@ -20,4 +20,13 @@ return {
   --   vim.cmd.colorscheme("catppuccin")
   -- end,
   -- },
+  -- NOTE:kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end,
+  },
 }
